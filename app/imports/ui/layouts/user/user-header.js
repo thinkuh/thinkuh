16a0e460c135a2e06fa0e2b48bdf59fc16589b7e
lyuyeda <lyuyeda@gmail.com>
2017-12-10 05:16:55 -1000

    Issue-39 User Header, Class Page box

--- app/imports/ui/layouts/user/user-header.js
+++ app/imports/ui/layouts/user/user-header.js
@@ -1,14 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-/*import { FlowRouter } from 'meteor/kadira:flow-router';*/
 import { Departments } from '/imports/api/department/DepartmentCollection';
 import { Majors } from '/imports/api/major/MajorCollection';
-
-Template.User_Header.helpers({
-  routeUserName() {
-    return Meteor.user().profile.name;
-  },
-});
+/*import { FlowRouter } from 'meteor/kadira:flow-router';*/
 
 Template.User_Header.onCreated(function onCreated() {
   this.subscribe(Departments.getPublicationName());
@@ -16,11 +10,20 @@ Template.User_Header.onCreated(function onCreated() {
 });
 
 Template.User_Header.helpers({
+  routeUserName() {
+    return Meteor.user().profile.name;
+  },
+  isUser() {
+    return Meteor.user();
+  },
   departments() {
     return Departments.find({}, { sort: { name: 1 } });
   },
   majors() {
     return Majors.find({}, { sort: { name: 1 } });
   },
+  findMajor(dmajor) {
+    return Majors._collection.findOne({ name: dmajor });
+  }
 });
 
