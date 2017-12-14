41b0ca3c1ec862a213525c20fe9a6d789b7b917e
Kirsten <khtakani@hawaii.edu>
2017-12-13 22:23:45 -1000

    changes to owl

--- app/imports/ui/pages/event/event-page.js
+++ app/imports/ui/pages/event/event-page.js
@@ -4,7 +4,10 @@ import { Events } from '/imports/api/event/EventCollection';
 Template.Event_Page.helpers({
   events() {
     return Events.find({}, { sort: { name: 1 } });
-  }
+  },
+  owl() {
+    $(".owl-carousel").owlCarousel();
+  },
 });
 
 Template.Event_Page.onCreated(function onCreated() {
